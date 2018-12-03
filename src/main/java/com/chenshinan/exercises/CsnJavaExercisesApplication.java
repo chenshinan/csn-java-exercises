9655b8a439b43aa88f41f334ca47a3103e3749b9
陈士男 <shinan.chen@hand-china.com>
2018-12-03 11:06:06 +0800

    [INIT] 初始化项目

---
+++ src/main/java/com/chenshinan/exercises/CsnJavaExercisesApplication.java
@@ -0,0 +1,12 @@
+package com.chenshinan.exercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsnJavaExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsnJavaExercisesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/chenshinan/exercises/CsnJavaExercisesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chenshinan.exercises;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CsnJavaExercisesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
