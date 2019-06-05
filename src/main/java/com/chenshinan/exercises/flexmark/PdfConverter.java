c71ec1ed7b7efad74ee4ca3726e9b9fbbf992d97
陈士男 <shinan.chen@hand-china.com>
2019-06-05 20:03:31 +0800

    [ADD] 新增flexmark

---
+++ src/main/java/com/chenshinan/exercises/flexmark/PdfConverter.java
@@ -0,0 +1,121 @@
+package com.chenshinan.exercises.flexmark;
+
+import com.vladsch.flexmark.ext.toc.TocExtension;
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.pdf.converter.PdfConverterExtension;
+import com.vladsch.flexmark.profiles.pegdown.Extensions;
+import com.vladsch.flexmark.profiles.pegdown.PegdownOptionsAdapter;
+import com.vladsch.flexmark.util.ast.Node;
+import com.vladsch.flexmark.util.options.MutableDataHolder;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+
+public class PdfConverter {
+    static final MutableDataHolder OPTIONS = PegdownOptionsAdapter.flexmarkOptions(
+            Extensions.ALL & ~(Extensions.ANCHORLINKS | Extensions.EXTANCHORLINKS_WRAP)
+            , TocExtension.create()).toMutable()
+            .set(TocExtension.LIST_CLASS, PdfConverterExtension.DEFAULT_TOC_LIST_CLASS)
+            //.set(HtmlRenderer.GENERATE_HEADER_ID, true)
+            //.set(HtmlRenderer.RENDER_HEADER_ID, true)
+            ;
+
+    static String getResourceFileContent(String resourcePath) {
+        StringWriter writer = new StringWriter();
+        getResourceFileContent(writer, resourcePath);
+        return writer.toString();
+    }
+
+    private static void getResourceFileContent(final StringWriter writer, final String resourcePath) {
+        InputStream inputStream = PdfConverter.class.getResourceAsStream(resourcePath);
+        try {
+            IOUtils.copy(inputStream, writer, "UTF-8");
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        final String pegdown = "" +
+                "[TOC]\n" +
+                "\n" +
+                "#Heading\n" +
+                "-----\n" +
+                "paragraph text \n" +
+                "lazy continuation\n" +
+                "\n" +
+                "* list item\n" +
+                "    > block quote\n" +
+                "    lazy continuation\n" +
+                "\n" +
+                "\n## Heading 2" +
+                "\n" +
+                "~~~info\n" +
+                "   with uneven indent\n" +
+                "      with uneven indent\n" +
+                "indented code\n" +
+                "~~~ \n" +
+                "\n" +
+                "\n## Heading 3" +
+                "\n" +
+                "       with uneven indent\n" +
+                "          with uneven indent\n" +
+                "    indented code\n" +
+                "1. numbered item 1   \n" +
+                "1. numbered item 2   \n" +
+                "1. numbered item 3   \n" +
+                "    - bullet item 1   \n" +
+                "    - bullet item 2   \n" +
+                "    - bullet item 3   \n" +
+                "        1. numbered 中国人   \n" +
+                "        1. numbered sub-item 2   \n" +
+                "        1. numbered sub-item 3   \n" +
+                "    \n" +
+                "    ~~~info\n" +
+                "       with uneven indent\n" +
+                "          with uneven indent\n" +
+                "    indented code\n" +
+                "    ~~~ \n" +
+                "    \n" +
+                "           with uneven indent\n" +
+                "              with uneven indent\n" +
+                "        indented code\n" +
+                "";
+        System.out.println("pegdown\n");
+        System.out.println(pegdown);
+
+        final Parser PARSER = Parser.builder(OPTIONS).build();
+        final HtmlRenderer RENDERER = HtmlRenderer.builder(OPTIONS).build();
+
+        Node document = PARSER.parse(pegdown);
+        String html = RENDERER.render(document);
+
+        // add embedded fonts for non-latin character set rendering
+        // change file:///usr/local/fonts/ to your system's path for font installation Unix path or
+        // on windows the path should start with `file:/X:/...` where `X:/...` is the drive
+        // letter followed by the full installation path.
+        //
+        // Google Noto fonts can be downloaded from https://www.google.com/get/noto/
+        // `arialuni.ttf` from https://www.wfonts.com/font/arial-unicode-ms
+//        String url = "'file:///Users/chenshinan/Downloads/arialuni.ttf'";
+        String url = "'" + PdfConverter.class.getResource("/arialuni.ttf") + "'";
+        String nonLatinFonts = "<style>\n" +
+                "@font-face {\n" +
+                "  font-family: 'arialuni';\n" +
+                "  src: url(" + url + ");\n" +
+                "}\n" +
+                "body {\n" +
+                "    font-family: 'arialuni';\n" +
+                "}\n" +
+                "</style>";
+        html = "<!DOCTYPE html><html><head><meta charset=\"UTF-8\">\n" + nonLatinFonts +
+                "</head><body>" + html + "\n" +
+                "</body></html>";
+
+        PdfConverterExtension.exportToPdf("./out14.pdf", html, "", OPTIONS);
+    }
+}
--- src/main/java/com/chenshinan/exercises/openhtmltopdf/PdfMain.java
+++ src/main/java/com/chenshinan/exercises/openhtmltopdf/PdfMain.java
@@ -1,5 +1,7 @@
 package com.chenshinan.exercises.openhtmltopdf;
 
+import com.openhtmltopdf.bidi.support.ICUBidiReorderer;
+import com.openhtmltopdf.bidi.support.ICUBidiSplitter;
 import com.openhtmltopdf.extend.FSSupplier;
 import com.openhtmltopdf.outputdevice.helper.BaseRendererBuilder;
 import com.openhtmltopdf.outputdevice.helper.FontFaceFontSupplier;
@@ -14,28 +16,53 @@ import java.io.*;
  */
 public class PdfMain {
     public static void main(String[] args) {
-        try (OutputStream os = new FileOutputStream("./out5.pdf")) { // 输出的pdf
+        try (OutputStream os = new FileOutputStream("./out10.pdf")) { // 输出的pdf
             PdfRendererBuilder builder = new PdfRendererBuilder();
             builder.useFastMode();
             //下面这个方法是要自己指定 字体文件   不然转出的pdf文件中 中文会变成####
-            builder.useFont(new FSSupplier<InputStream>() {
-                @Override
-                public InputStream supply() {
-                    try {
-                        return new FileInputStream("./simhei.ttf");
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    }
-                    return null;
+            builder.useFont(()->{
+                try {
+                    return new FileInputStream("./NotoSansCJKtc-Regular.ttf");
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
                 }
-            }, "simhei", 400, BaseRendererBuilder.FontStyle.NORMAL, true); //第二个参数 一定要和文件名一样！！作用在html页面上
-//            builder.withHtmlContent("<style>\n" +
-//                    "* {\n" +
-//                    "  font-family: 'simhei';   //value是指定字体时 第二个参数\n" +
-//                    "}\n" +
-//                    "</style><h2>acaca1在哪里</h2>","./root.htm");
-//            builder.useFastMode();
-            builder.withUri("file:///Users/chenshinan/Downloads/root.htm");
+                return null;
+            }, "cjk", 400, BaseRendererBuilder.FontStyle.NORMAL, true); //第二个参数 一定要和文件名一样！！作用在html页面上
+            String nonLatinFonts = "" +
+                    "<style>\n" +
+                    "@font-face {\n" +
+                    "  font-family: 'simhei';\n" +
+                    "  src: url('file:///Users/chenshinan/Downloads/simhei.ttf');\n" +
+                    "  font-weight: normal;\n" +
+                    "  font-style: normal;\n" +
+                    "}\n" +
+                    "body {\n" +
+                    "    font-family: 'simhei';\n" +
+                    "    overflow: hidden;\n" +
+                    "    word-wrap: break-word;\n" +
+                    "    font-size: 14px;\n" +
+                    "}\n" +
+                    "\n" +
+                    "var,\n" +
+                    "code,\n" +
+                    "kbd,\n" +
+                    "pre {\n" +
+                    "    font: 0.9em 'noto-mono', Consolas, \"Liberation Mono\", Menlo, Courier, monospace;\n" +
+                    "}\n" +
+                    "</style>\n" +
+                    "";
+
+            String html = "<!DOCTYPE html><html><head><meta charset=\"UTF-8\"/>\n" +
+                    "" +  nonLatinFonts +// add your stylesheets, scripts styles etc.
+                    // uncomment line below for adding style for custom embedded fonts
+                    // nonLatinFonts +
+                    "</head><body>" + "中国人" + "\n" +
+                    "</body></html>";
+            builder.withHtmlContent(html,"./root.htm");
+            builder.useUnicodeBidiSplitter(new ICUBidiSplitter.ICUBidiSplitterFactory());
+            builder.useUnicodeBidiReorderer(new ICUBidiReorderer());
+            builder.defaultTextDirection(BaseRendererBuilder.TextDirection.LTR);
+//            builder.withUri("file:///Users/chenshinan/Downloads/root.htm");
             //第一个参数是html页面  第二个参数 是一个全空的文件 里面什么都没有 但是后缀一定要是。htm  作用类似于一个画板 如果不添加这个参数或者置为null 则html文件中的图片 不会转化 所以一定需要加
             builder.toStream(os);
             builder.run();
